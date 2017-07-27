82f4b6598a356eafa7a1227cd099c4412172bf06
Tom Roggero <tomas.roggero@rga.com>
2017-07-26 22:55:31 -0400

    Progress

--- sw.js
+++ sw.js
@@ -1,13 +1,12 @@
 self.addEventListener('push', evt => {
-    console.log('[Service Worker] Push Received.');
-    console.log(`[Service Worker] Push had this data: "${evt.data.text()}"`);
 
     const title = 'Push Codelab';
     const options = {
-        body: 'Yay it works.',
+        body: evt.data.text(),
         icon: 'images/icon.png',
         badge: 'images/badge.png'
     };
 
     evt.waitUntil(self.registration.showNotification(title, options));
+
 });
