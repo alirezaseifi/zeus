6fa323903334178a05647fad0485d40888250c7b
Tom Roggero <tomas.roggero@rga.com>
2017-07-26 20:10:26 -0400

    lowercase .env file

--- server.js
+++ server.js
@@ -4,3 +4,30 @@
 // .then((keys) => {
 //   return cryptoKeyToUrlBase64(keys.publicKey, keys.privateKey);
 // });
+
+const webpush = require('web-push');
+
+// VAPID keys should only be generated only once.
+const vapidKeys = webpush.generateVAPIDKeys();
+
+console.log(vapidKeys);
+
+webpush.setGCMAPIKey(process.env.SERVER_KEY);
+webpush.setVapidDetails(
+    'mailto:tomasloon@gmail.com',
+    vapidKeys.publicKey,
+    vapidKeys.privateKey
+);
+
+
+
+// This is the same output of calling JSON.stringify on a PushSubscription
+// const pushSubscription = {
+//   endpoint: '.....',
+//   keys: {
+//     auth: '.....',
+//     p256dh: '.....'
+//   }
+// };
+
+// webpush.sendNotification(pushSubscription, 'Your Push Payload Text');
