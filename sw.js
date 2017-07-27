7e305d9e2c3fbbb2f088a379e25307a7328e91a4
Tom Roggero <tomas.roggero@rga.com>
2017-07-27 02:55:21 -0400

    Alpha v1

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
 self.addEventListener('push', evt => {
 
-    const title = 'Push Codelab';
-    const options = {
-        body: evt.data.text(),
-        icon: 'images/icon.png',
-        badge: 'images/badge.png'
-    };
+    const { body, url } = JSON.parse(evt.data.text());
 
-    evt.waitUntil(self.registration.showNotification(title, options));
+    evt.waitUntil(self.registration.showNotification('ZEUS', {
+        body,
+        data: { url }
+    }));
 
 });
+
+self.addEventListener('notificationclick', evt => evt.waitUntil(clients.openWindow(evt.notification.data.url)));
